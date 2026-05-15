646a74acdd4a488d6ef1724434e13739b483f3e5
amaanmirza1 <ammirxaa@gmail.com>
2026-05-15 13:44:45 +0530

    final deployment

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://linenaura.onrender.com";
 
 export async function getProducts() {
   const response = await fetch(
